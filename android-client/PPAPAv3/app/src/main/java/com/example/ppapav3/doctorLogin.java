a67980a16d3d74c1093b66da86a1e5ae4f6cb6e7
seog14 <36527388+seog14@users.noreply.github.com>
2021-03-25 02:21:21 -0400

    update android app

--- android-client/PPAPAv3/app/src/main/java/com/example/ppapav3/doctorLogin.java
+++ android-client/PPAPAv3/app/src/main/java/com/example/ppapav3/doctorLogin.java
@@ -8,8 +8,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class doctorLogin extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.ppapav3.dto.AppResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class doctorLogin extends AppCompatActivity {
+    private static String url ="https://cs3zhrwmnc.execute-api.us-east-1.amazonaws.com/APILoginCheck";
     private EditText Name;
     private EditText Password;
     private TextView Info;
@@ -19,7 +28,7 @@ public class doctorLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doctor_login);
+        setContentView(R.layout.activity_pharm_login);
 
         Name = (EditText)findViewById(R.id.etPatient);
         Password = (EditText)findViewById(R.id.etPatientPass);
@@ -31,27 +40,92 @@ public class doctorLogin extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                JSONObject jsonObj;
+                try {
+                    jsonObj = new JSONObject();
+                    jsonObj.put("user", Name.getText().toString());
+                    jsonObj.put("pass", Password.getText().toString());
+                } catch (JSONException e){
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, jsonObj, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                AppResponse response1 = AppResponse.newInstance(AppResponse.class,  response.toString());
+                                System.out.println("response = " + response.toString());
+                                if(response1.getStatus() == 1) {
+                                    Intent intent = new Intent(doctorLogin.this, doctorMainPage.class);
+                                    startActivity(intent);
+                                }
+                                else {
+                                    counter--;
+                                    Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
+
+                                    if (counter == 0)
+                                    {
+                                        Login.setEnabled(false);
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+
+                            }
+                        });
+//                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+////                                JSONParser parser = new JSONParser();
+////                                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+////                                JSONObject responseJson = new JSONObject();
+//                                AppResponse response1 = AppResponse.newInstance(AppResponse.class,  response);
+//                                System.out.println("response = " + response);
+//
+//
+//                               // Intent intent = new Intent(pharmLogin.this, pharmMainPage.class);
+//                             //   startActivity(intent);
+//
+//                                // Display the first 500 characters of the response string.
+//                                //textView.setText("Response is: "+ response.substring(0,500));
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        System.out.println("ffdfdfdfdf error" );
+//                        error.printStackTrace();
+//                    }
+//                });
+                RQueueSingleton.getInstance(getApplicationContext()).getRequestQueue().add(jsonObjectRequest);
+
             }
+
+
         });
     }
-    private void validate(String userName, String userPassword)
-    {
-        if ((userName.equals("Admin")) && (userPassword.equals("Password")))
-        {
-            Intent intent = new Intent(doctorLogin.this, doctorMainPage.class);
-            startActivity(intent);
-        }
-        else
-        {
-            counter--;
-            Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
+//    private void validate()
+//    {
+//        if (status)
+//        {
+//            Intent intent = new Intent(pharmLogin.this, pharmMainPage.class);
+//            startActivity(intent);
+//        }
+//        else
+//        {
+//            counter--;
+//            Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
+//
+//            if (counter == 0)
+//            {
+//                Login.setEnabled(false);
+//            }
+//        }
+//    }
 
-            if (counter == 0)
-            {
-                Login.setEnabled(false);
-            }
-        }
-    }
 
 }
--- android-client/PPAPAv3/app/src/main/java/com/example/ppapav3/patientLogin.java
+++ android-client/PPAPAv3/app/src/main/java/com/example/ppapav3/patientLogin.java
@@ -8,6 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.ppapav3.dto.AppResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class patientLogin extends AppCompatActivity {
 
     private EditText Name;
@@ -15,11 +24,12 @@ public class patientLogin extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private int counter = 5;
+    private static String url ="https://cs3zhrwmnc.execute-api.us-east-1.amazonaws.com/APILoginCheck";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patient_login);
+        setContentView(R.layout.activity_pharm_login);
 
         Name = (EditText)findViewById(R.id.etPatient);
         Password = (EditText)findViewById(R.id.etPatientPass);
@@ -31,28 +41,92 @@ public class patientLogin extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                JSONObject jsonObj;
+                try {
+                    jsonObj = new JSONObject();
+                    jsonObj.put("user", Name.getText().toString());
+                    jsonObj.put("pass", Password.getText().toString());
+                } catch (JSONException e){
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, jsonObj, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                AppResponse response1 = AppResponse.newInstance(AppResponse.class,  response.toString());
+                                System.out.println("response = " + response.toString());
+                                if(response1.getStatus() == 1) {
+                                    Intent intent = new Intent(patientLogin.this, patientMainPage.class);
+                                    startActivity(intent);
+                                }
+                                else {
+                                    counter--;
+                                    Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
+
+                                    if (counter == 0)
+                                    {
+                                        Login.setEnabled(false);
+                                    }
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+
+                            }
+                        });
+//                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+////                                JSONParser parser = new JSONParser();
+////                                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+////                                JSONObject responseJson = new JSONObject();
+//                                AppResponse response1 = AppResponse.newInstance(AppResponse.class,  response);
+//                                System.out.println("response = " + response);
+//
+//
+//                               // Intent intent = new Intent(pharmLogin.this, pharmMainPage.class);
+//                             //   startActivity(intent);
+//
+//                                // Display the first 500 characters of the response string.
+//                                //textView.setText("Response is: "+ response.substring(0,500));
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        System.out.println("ffdfdfdfdf error" );
+//                        error.printStackTrace();
+//                    }
+//                });
+                RQueueSingleton.getInstance(getApplicationContext()).getRequestQueue().add(jsonObjectRequest);
+
             }
+
+
         });
     }
-    private void validate(String userName, String userPassword)
-    {
-        if ((userName.equals("Admin")) && (userPassword.equals("Password")))
-        {
-            Intent intent = new Intent(patientLogin.this, patientMainPage.class);
-            startActivity(intent);
-        }
-        else
-        {
-            counter--;
-            Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
-
-            if (counter == 0)
-            {
-                Login.setEnabled(false);
-            }
-        }
-    }
+//    private void validate()
+//    {
+//        if (status)
+//        {
+//            Intent intent = new Intent(pharmLogin.this, pharmMainPage.class);
+//            startActivity(intent);
+//        }
+//        else
+//        {
+//            counter--;
+//            Info.setText("# of Attempts Remaining: " + String.valueOf(counter));
+//
+//            if (counter == 0)
+//            {
+//                Login.setEnabled(false);
+//            }
+//        }
+//    }
 
 
 }
